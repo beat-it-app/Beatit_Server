1bc50641c17a811f0285c8619e3d7d1337f05516
Youngseo <ysnoh22@naver.com>
2026-05-12 18:05:57 +0900

    feat/#9: 기본 파일 생성 및 테이블 생성

---
+++ src/main/kotlin/com/beat_it/auth/entity/UserProfiles.kt
@@ -0,0 +1,11 @@
+package com.beat_it.auth.entity 
+
+import jakarta.persistence.*
+import java.time.OffsetDateTime
+import java.util.UUID
+
+@Entity
+@Table(name = "user_profiles")
+class UserProfiles(
+    
+)
