6390660a8d15d65bdf23c27c7184091dd68b6e81
EunHaSong <shaeun03290@sookmyung.ac.kr>
2026-05-27 21:10:12 +0900

    feat/#12: userId JWT에서 발급 받아서 사용

--- src/main/kotlin/com/beat_it/team/controller/TeamController.kt
+++ src/main/kotlin/com/beat_it/team/controller/TeamController.kt
@@ -7,11 +7,14 @@ import com.beat_it.team.dto.TeamDetailUpdateRequest
 import com.beat_it.team.dto.TeamDetailUpdateResponse
 import com.beat_it.team.service.TeamService
 import com.beat_it.global.response.BasicResponse
+import com.beat_it.global.security.SecurityUtil
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.UUID
 import io.swagger.v3.oas.annotations.Parameter
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.UserDetails
 
 @RestController
 @RequestMapping("/teams")
@@ -21,12 +24,11 @@ class TeamController(
 
     @PostMapping
     fun createTeam(
-        @Parameter(hidden = true)
-        @RequestHeader("X-User-Public-Id") userPublicId: UUID,
+        @AuthenticationPrincipal userDetails: UserDetails?,
 
         @RequestBody request: TeamCreateRequest
     ): ResponseEntity<BasicResponse<TeamCreateResponse>> {
-
+        val userPublicId = UUID.fromString(userDetails?.username)
         val responseData = teamService.createTeam(userPublicId, request)
 
         return ResponseEntity
@@ -36,28 +38,27 @@ class TeamController(
 
     @PatchMapping
     fun updateTeamDetail(
-        @Parameter(hidden = true)
-        @RequestHeader("X-User-Public-Id") userPublicId: UUID,
-
+        @AuthenticationPrincipal userDetails: UserDetails?,
         @Parameter(hidden = true)
         @RequestHeader("X-Team-Public-Id") teamPublicId: UUID,
 
         @RequestBody request: TeamDetailUpdateRequest,
     ): ResponseEntity<BasicResponse<TeamDetailUpdateResponse>> {
+        val userPublicId = UUID.fromString(userDetails?.username)
         val responseData = teamService.updateTeamDetail(teamPublicId, userPublicId, request)
+
         return ResponseEntity.ok(BasicResponse.success(responseData, HttpStatus.OK, "팀 상세 내용이 수정되었습니다."))
     }
 
     @DeleteMapping
     fun deleteTeam(
-        @Parameter(hidden = true)
-        @RequestHeader("X-User-Public-Id") userPublicId: UUID,
-
+        @AuthenticationPrincipal userDetails: UserDetails?,
         @Parameter(hidden = true)
         @RequestHeader("X-Team-Public-Id") teamPublicId: UUID,
-
     ): ResponseEntity<BasicResponse<Nothing>> {
+        val userPublicId = UUID.fromString(userDetails?.username)
         teamService.deleteTeam(teamPublicId, userPublicId)
+
         return ResponseEntity.ok(
             BasicResponse.success(HttpStatus.OK,"팀이 성공적으로 삭제되었습니다.")
         )
@@ -67,7 +68,6 @@ class TeamController(
     fun getTeamDetail(
         @Parameter(hidden = true)
         @RequestHeader("X-Team-Public-Id") teamPublicId: UUID,
-
     ): ResponseEntity<BasicResponse<TeamDetailResponse>> {
         val responseData = teamService.getTeamDetail(teamPublicId)
         return ResponseEntity.ok(
