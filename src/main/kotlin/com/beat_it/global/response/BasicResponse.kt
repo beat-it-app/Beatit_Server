3458701b89a0eb144129baf3b837d82e8d1358b0
Youngseo <ysnoh22@naver.com>
2026-06-08 17:10:45 +0900

    fix/#25: BaseRespones true인 경우, 상태코드 string 말고 int로 받도록

--- src/main/kotlin/com/beat_it/global/response/BasicResponse.kt
+++ src/main/kotlin/com/beat_it/global/response/BasicResponse.kt
@@ -4,16 +4,16 @@ import org.springframework.http.HttpStatus
 
 data class BasicResponse<T>(
     val success: Boolean,
-    val status: String,
+    val status: Any,
     val message: String?,
     val data: T?
 ) {
     companion object {
         fun <T> success(data: T, httpStatus: HttpStatus, message: String? = "Request Successful"): BasicResponse<T> =
-            BasicResponse(true, httpStatus.value().toString(), message, data)
+            BasicResponse(true, httpStatus.value(), message, data)
 
         fun success(httpStatus: HttpStatus, message: String?): BasicResponse<Nothing> =
-            BasicResponse(true, httpStatus.value().toString(), message, null)
+            BasicResponse(true, httpStatus.value(), message, null)
 
         fun fail(code: String, message: String): BasicResponse<Nothing> =
             BasicResponse(false, code, message, null)
