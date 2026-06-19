0b485f482c077278032fa0788c9eef4109e37cc0
송하은 <155309085+EunHaSong@users.noreply.github.com>
2026-06-19 19:34:13 +0900

    feat/#26: 연습실, 댓글, 반응 레포지토리 추가

---
+++ src/main/kotlin/com/beat_it/team/repository/ArchiveCommentsRepository.kt
@@ -0,0 +1,14 @@
+package com.beat_it.team.repository
+
+import com.beat_it.team.entity.ArchiveComments
+import com.beat_it.team.entity.Archives
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ArchiveCommentsRepository : JpaRepository<ArchiveComments, Long> {
+    fun findByArchiveId(archiveId: Long): Archives?
+
+    fun deleteByArchiveArchiveId(archiveId: Long): Int
+
+}
---
+++ src/main/kotlin/com/beat_it/team/repository/ArchiveRepository.kt
@@ -0,0 +1,12 @@
+package com.beat_it.team.repository
+
+import com.beat_it.team.entity.Archives
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ArchiveRepository : JpaRepository<Archives, Long> {
+    fun findByArchiveId(archiveId: Long): Archives?
+
+    fun findByArchiveIdOrNull(archiveId: Long): Archives?
+}
