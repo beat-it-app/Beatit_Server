09ecc1b76d8528722e14e35fff57b410bbfd0525
jiwonniddaaa <jiwonniddaaa02@gmail.com>
2026-08-22 21:48:21 +0900

    fix/#78: index id명 통일

--- src/main/kotlin/com/beat_it/chat/entity/ChatMessage.kt
+++ src/main/kotlin/com/beat_it/chat/entity/ChatMessage.kt
@@ -7,7 +7,7 @@ import jakarta.persistence.*
 @Table(
     name = "chat_messages",
     indexes = [
-        Index(name = "idx_chat_room_created_at", columnList = "chat_room_id, created_at DESC")
+        Index(name = "idx_chat_room_created_at", columnList = "chat_id, created_at DESC")
     ]
 )
 class ChatMessage(
