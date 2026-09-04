f2009324561d03ead6ec0debba643a29d354b86e
youngseo22 <ysnoh22@sookmyung.ac.kr>
2026-09-04 09:37:57 +0900

    [HOTFIX] 누락된 offSetDateTime 변경

--- src/main/kotlin/com/beat_it/team/dto/TeamWithdrawalResponse.kt
+++ src/main/kotlin/com/beat_it/team/dto/TeamWithdrawalResponse.kt
@@ -1,10 +1,11 @@
 package com.beat_it.team.dto
 
+import java.time.OffsetDateTime
 import java.util.UUID
 
 data class TeamWithdrawalResponse(
     val teamPublicId: UUID,
     val userId: Long,
-    val requestedAt: String,
-    val scheduledDeletionDate: String
+    val requestedAt: OffsetDateTime,
+    val scheduledDeletionDate: OffsetDateTime
 )
